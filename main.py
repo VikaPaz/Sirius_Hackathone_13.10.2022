a1c77a38c7461de9b566c6f10993e21fb0b8dd25
VikaPaz <vikaqse@gmail.com>
2022-10-14 10:02:59 +0300

    'commit2'

--- main.py
+++ main.py
@@ -5,97 +5,68 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog, QFileDialog
     QTableWidgetItem, QPushButton, QInputDialog
 import sys
 
-from yargy import rule, Parser
-from yargy.predicates import gram, dictionary, normalized
-from yargy.pipelines import morph_pipeline
-from openpyxl import load_workbook
-
-
-
-con = sqlite3.connect("test.db")
-
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('1.ui', self)
 
+        self.con = sqlite3.connect("course_base.db")
+
         self.lineEdit.textChanged[str].connect(self.new_text)
-        self.pushButton.clicked.connect(self.search_courses)
+        self.pushButton.clicked.connect(self.search_competences)
+        self.comboBox.activated[str].connect(self.new_itm)
 
         self.position = ''
         self.lst_courses = []
 
-        self.updata_tw()
+        self.item = ''
+        self.update_tableWidget()
 
-    def new_text(self, text) -> None:
+        self.position = {}
+
+    def new_text(self, text):
         self.position = text
-        print(self.position)
+        # print(self.position)
+
+    def search_competences(self):
+        cur = self.con.cursor()
+        val = cur.execute(f"SELECT name FROM jobs").fetchall()
+        print(val)
+        val = [i[0] for i in val]
+        if self.position in val:
+            result = cur.execute(f'SELECT list FROM jobs WHERE name = "{self.position}"').fetchall()
+            result = result[0][0].split('/')
+        self.competencies = [''] + result
+        self.update_comboBox()
+
+    def update_comboBox(self):
+        self.comboBox.addItems(self.competencies)
+
+    def new_itm(self, item):
+        self.item = item
+        self.search_courses()
 
     def search_courses(self):
-        R1 = rule(gram('ADJF').optional(), gram('NOUN'))  # rule searching all key pairs ADJECTIVE NOUN
-        parser = Parser(R1)
-
-        comps = load_workbook('competences.xlsx')  # opening xlsx table as data source
-        first = comps['Лист1']
-        c = int(input())
-        print(first[f'B{c}'].value)
-        text = first[f'C{c}'].value
-
-        self.res = set()
-        for item in parser.findall(text):  # getting simple 'keywords' from table cell
-            self.res.add(' '.join([_.value for _ in item.tokens]))
-        print(self.res)
-
-        R2 = morph_pipeline(list(self.res))  # rule searching matches by keywords in desc
-        self.parser1 = Parser(R2)
-
-        courses = load_workbook('courses.xlsx')  # loading page with courses' descriptions
-        self.page = courses['Описание курсов']
-        self.find_all_courses()
-        self.updata_tw()
-
-    def find_all_courses(self) -> set:
-        list_of_matched = []  # resulting list
-        for i in range(2, 500):
-            list_of_matched.clear()  # cleaning list
-            desc = self.page[f'B{i}'].value  # setting value of considered self.page
-
-            for match in self.parser1.findall(desc):  # parsing
-                list_of_matched.append(' '.join([_.value if len(_.value) > 1 else '' for _ in match.tokens]))
-
-            if len(self.res.intersection(list_of_matched)) > len(
-                    list_of_matched) // 10:  # checking if more than 20% of keywords are equal
-                # print(list_of_matched)
-                print(self.page[f'A{i}'].value)  # name of course
-
-            # print(len(res.intersection(list_of_matched)) > len(res)//10)
-            # print(page[f'A{i}'].value)  # name of course
-
-        return set(list_of_matched)
-
-    def updata_tw(self):
-        self.con = sqlite3.connect("Сourses.db")
         cur = self.con.cursor()
-        result = cur.execute("SELECT * FROM competencies").fetchall()
-        print(result)
-        self.tableWidget.setRowCount(len(result))
-        self.tableWidget.setColumnCount(len(result[0]))
-        for i, elem in enumerate(result):
-            for j, val in enumerate(elem):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        # для ex
-        '''
-                self.result = [('', '')]
-        # парс таблици
-        self.result = self.result
-        
-        self.tableWidget.setRowCount(len(self.result))
-        self.tableWidget.setColumnCount(len(self.result[0]))
-        for i, elem in enumerate(self.result):
-            for j, val in enumerate(elem):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
-        '''
+
+        val = cur.execute(f"SELECT comp FROM final").fetchall()
+        val = [i[0] for i in val]
+        print(val)
+        print(self.item)
+        if self.item in val:
+            val = cur.execute(f'SELECT courses FROM final WHERE comp = "{self.item}"').fetchall()
+            val = val[0][0].split('/')
+            print(val)
+        self.lst_courses = val
+        self.update_tableWidget()
+
+    def update_tableWidget(self):
+        if self.item != '':
+            print(len(self.lst_courses))
+            self.tableWidget.setRowCount(len(self.lst_courses))
+            for i, val in enumerate(self.lst_courses):
+                self.tableWidget.setItem(i, 0, QTableWidgetItem(str(val)))
 
 
 if __name__ == '__main__':
